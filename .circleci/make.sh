229d151abb39cfe334ef6dcaad463233c2e0a96f
cibuilders <55670096+cibuilders@users.noreply.github.com>
2019-09-23 05:43:17 +0700

    Update make.sh

--- .circleci/make.sh
+++ .circleci/make.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-
+sudo apt-get update -y
+sudo apt-get install -y axel
 cd /tmp || exit 1
-axel -a -n 10 https://ftp.gnu.org/gnu/make/make-"${1:?}".tar.gz
-tar xvzf /tmp/make-"${1:?}".tar.gz
-cd /tmp/make-"${1:?}" || exit 1
+axel -a -n 10 https://ftp.gnu.org/gnu/make/make-4.2.tar.gz
+tar xvzf /tmp/make-4.2.tar.gz
+cd /tmp/make-4.2 || exit 1
 ./configure
 curl https://raw.githubusercontent.com/akhilnarang/scripts/master/patches/make-glibc_alloc_fix.patch | patch -p1
 bash ./build.sh
 sudo install ./make /usr/local/bin/make
 cd - || exit 1
-rm -rf /tmp/make-"${1:?}"{,.tar.gz}
+rm -rf /tmp/make-4.2*
